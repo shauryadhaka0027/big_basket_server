f7438f9d2b87987eee9e55e89e473d208a13dda3
shaurya2014kira <166789175+shaurya2014kira@users.noreply.github.com>
2024-10-14 17:24:26 +0530

    new

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://big-basket-client.vercel.app",
     credentials: true,
     methods: ['GET', 'POST', 'DELETE', 'PATCH']
 }))
